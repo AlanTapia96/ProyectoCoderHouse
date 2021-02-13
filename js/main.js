62d19fa1943a65061146418b63a93fbeb1b44bd3
= <=>
2021-02-12 21:09:58 -0300

    Modificacion DOM - Muestra carrito de compras

--- js/main.js
+++ js/main.js
@@ -43,8 +43,8 @@ const iva = 0.21;
 
 const camisetasArgentina = ["Boca Juniors","River Plate","Independiente","Racing","San Lorenzo","Vélez Sarsfield"];
 const camisetasBrasil = ["Santos","Palmeiras","Flamengo","Fluminense","Gremio","Corinthians"];
-const camisetasEspaña = ["Barcelona","Real Madrid","Atlético de Madrid","Sevilla","Valencia","Villareal"];
-const camisetasItalia = ["Juventus","Milan","Inter","Nápoli","Roma","Atlanta"];
+const camisetasEspaña = ["Barcelona","Real Madrid","Atlético de Madrid"];
+const camisetasItalia = ["Juventus","Milan","Inter","Nápoli"];
 
 const stockArgentina = [];
 const stockBrasil = [];
@@ -56,8 +56,8 @@ crearCamisetas(camisetasBrasil,stockBrasil,"Brasil",30,"m");
 crearCamisetas(camisetasEspaña,stockEspaña,"España",30,"m");
 crearCamisetas(camisetasItalia,stockItalia,"Italia",30,"m");
 
-carrito = [];
-importe = [];
+let carritoDeCompras = [];
+let importe = [];
 
 /*****************************************************************************************************************/
 
@@ -97,13 +97,8 @@ function aumento(lista,aumento) {
     lista.map(camiseta => camiseta.modificarPrecio(camiseta.devolverPrecio()*aumento));    
 }
 
-aumento(stockArgentina,1.20);
-aumento(stockBrasil,1.20);
-aumento(stockEspaña,1.20);
-aumento(stockItalia,1.20);
 
-
-function eleccion() {
+function eleccion(etapa) {
     
     let opcionInicial = prompt("Por favor, indique el país al cual pertenece el equipo que desea la camiseta de fútbol:\n1. Argentina \n2. Brasil\n3. España\n4. Italia\n");
     while (opcionInicial < 1 || opcionInicial > 4 || opcionInicial == ""){
@@ -154,39 +149,64 @@ function eleccion() {
         opcionCamiseta = stockEspaña[parseInt(opcionCamiseta) - 1].devolverClub();
         precio = costoCamiseta(stockArgentina.find(camiseta => camiseta.devolverClub()).devolverPrecio());
     }
-    carrito.push(opcionCamiseta);
+    carritoDeCompras.push(opcionCamiseta);
     importe.push(precio);
     
     alert("Usted eligió la camiseta de " + opcionCamiseta + " por un precio total de " + precio);
 
     continuar = prompt("Desea comprar otra camiseta? (S/N)")
     if(continuar == "S" || continuar == "s"){
-        eleccion();
+        eleccion(etapa+1);
     }
-
-    continuar = prompt("Desea ver su carrito de compras? (S/N)")
-    if(continuar == "S" || continuar == "s"){
-        let compra = "";
-        for (let i = 0; i < carrito.length; i++) {
-            compra += carrito[i] + ": " + importe[i] + "\n"
-            
+    if(etapa===0){
+        continuar = prompt("Desea ver su carrito de compras? (S/N)")
+        if(continuar == "S" || continuar == "s"){
+            mostrarCarrito(carritoDeCompras,importe);
+        }else{
+            alert("Muchas gracias por su visita!");
         }
-        let costoTotal = importe.reduce((a,b) => a + b,0);
-      alert(compra + "\n" + "Total: " + costoTotal);  
-    }else{
-        alert("Muchas gracias por su visita!");
     }
-
 
 }
 
+function generarCamisetasEnHTML(listaCamisetas){
+    let camisetas = document.getElementById("camisetas");
+    let contenedor = document.createElement("div");
+    for (const camiseta of listaCamisetas) {
+        let li = document.createElement("li");
+        li.innerHTML = `La camiseta de ${camiseta.club} cuesta ${camiseta.devolverPrecio()*(iva+1)}`;
+        camisetas.appendChild(li);
+    }
+}
 
 
-
+function mostrarCarrito(listaCompras,listaImporte){
+    let carrito = document.getElementById("carrito");
+    for (let i = 0; i < listaCompras.length; i++) {
+        let contenedor = document.createElement("div");
+        let p = document.createElement("p");
+        p.innerHTML = `Camiseta: ${listaCompras[i]} - Precio: ${listaImporte[i]}`
+        contenedor.appendChild(p);
+        carrito.appendChild(contenedor);
+    }
+        
+}
 
 
 /*********************************** MAIN ***********************************/
 alert("¡ Bienvenido a la tienda de camisetas de fútbol !");
-eleccion();
+aumento(stockArgentina,1.20);
+aumento(stockBrasil,1.20);
+aumento(stockEspaña,1.20);
+aumento(stockItalia,1.20);
+eleccion(0);
 
 
+
+
+/*
+generarCamisetasEnHTML(stockArgentina);
+generarCamisetasEnHTML(stockBrasil);
+generarCamisetasEnHTML(stockEspaña);
+generarCamisetasEnHTML(stockItalia);
+*/
